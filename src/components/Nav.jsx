690e66696cb10b872244b9d1de0baba48e74df3d
yu.wang <wangyu@wycode.cn>
2024-12-11 18:16:28 +0800

    move to nextjs

---
+++ src/components/Nav.jsx
@@ -0,0 +1,41 @@
+'use client'
+import { usePathname } from "next/navigation";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/navbar"
+import Link from "next/link";
+export function Nav() {
+    const pathname = usePathname();
+
+    console.log(pathname);
+    return (
+        <Navbar isBordered>
+            <NavbarBrand>
+                <Link href="/" className="font-bold">Home</Link>
+            </NavbarBrand>
+            <NavbarContent>
+                <NavbarItem>
+                    <Link href="/x6" className={`${pathname === "/x6" ? "font-bold" : ""} text-sky-600`}>X6绘图</Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent>
+                <NavbarItem>
+                    <Link href="/echarts" className={`${pathname === "/echarts" ? "font-bold" : ""} text-sky-600`}>ECharts图表</Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent>
+                <NavbarItem>
+                    <Link href="/echarts-dataset" className={`${pathname === "/echarts-dataset" ? "font-bold" : ""} text-sky-600`}>ECharts数据集</Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent>
+                <NavbarItem>
+                    <Link href="/gdp" className={`${pathname === "/gdp" ? "font-bold" : ""} text-sky-600`}>ECharts-GDP</Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent>
+                <NavbarItem>
+                    <Link href="/amis" className={`${pathname === "/amis" ? "font-bold" : ""} text-sky-600`}>Amis</Link>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    )
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,12 @@
+import { nextui } from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -13,5 +16,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
